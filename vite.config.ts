89167e4dda586eb40ede4e5af4eb57a364f2ea44
Rommel <2022307038@pampangastateu.edu.ph>
2025-09-17 04:12:21 +0800

    fix login

--- vite.config.ts
+++ vite.config.ts
@@ -14,13 +14,13 @@ export default defineConfig({
         react(),
         tailwindcss(),
     ],
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        hmr: {
-            host: '192.168.100.20',
-        },
-    },
+    // server: {
+    //     host: '0.0.0.0',
+    //     port: 5173,
+    //     hmr: {
+    //         host: '192.168.100.20',
+    //     },
+    // },
 
     esbuild: {
         jsx: 'automatic',
