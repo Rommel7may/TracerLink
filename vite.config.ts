2ad9bad3b47643a2a0356e898c7a191df4c04319
Rommel7may <ml0997130@gmail.com>
2025-09-18 15:11:17 +0800

    removed docker supervisor

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,39 @@
+// import tailwindcss from '@tailwindcss/vite';
+// import react from '@vitejs/plugin-react';
+// import laravel from 'laravel-vite-plugin';
+// import { resolve } from 'node:path';
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.tsx'],
+//             ssr: 'resources/js/ssr.tsx',
+//             refresh: true,
+//         }),
+//         react(),
+//         tailwindcss(),
+//     ],
+//     server: {
+//         host: '0.0.0.0',
+//         port: 5173,
+//         hmr: {
+//             host: '127.0.0.1',
+//         },
+//     },
+
+//     esbuild: {
+//         jsx: 'automatic',
+//     },
+//     resolve: {
+//         alias: {
+//             'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
+//         },
+//     },
+// });
+
+
+
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
@@ -8,22 +44,20 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
-            ssr: 'resources/js/ssr.tsx',
             refresh: true,
         }),
         react(),
         tailwindcss(),
     ],
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        hmr: {
-            host: '127.0.0.1',
+    build: {
+        outDir: 'public/build',
+        emptyOutDir: true,
+        manifest: true, // ensures Laravel can find the assets
+        rollupOptions: {
+            input: {
+                app: resolve(__dirname, 'resources/js/app.tsx'),
+            },
         },
-    },
-
-    esbuild: {
-        jsx: 'automatic',
     },
     resolve: {
         alias: {
