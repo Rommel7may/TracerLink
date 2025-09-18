d134a79a393768092d4c7f28f1c4b43dd4110b4e
Rommel <2022307038@pampangastateu.edu.ph>
2025-09-18 15:25:12 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,3 @@
-// import tailwindcss from '@tailwindcss/vite';
-// import react from '@vitejs/plugin-react';
-// import laravel from 'laravel-vite-plugin';
-// import { resolve } from 'node:path';
-// import { defineConfig } from 'vite';
-
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: ['resources/css/app.css', 'resources/js/app.tsx'],
-//             ssr: 'resources/js/ssr.tsx',
-//             refresh: true,
-//         }),
-//         react(),
-//         tailwindcss(),
-//     ],
-//     server: {
-//         host: '0.0.0.0',
-//         port: 5173,
-//         hmr: {
-//             host: '127.0.0.1',
-//         },
-//     },
-
-//     esbuild: {
-//         jsx: 'automatic',
-//     },
-//     resolve: {
-//         alias: {
-//             'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
-//         },
-//     },
-// });
-
-
-
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
@@ -44,20 +8,22 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
+            ssr: 'resources/js/ssr.tsx',
             refresh: true,
         }),
         react(),
         tailwindcss(),
     ],
-    build: {
-        outDir: 'public/build',
-        emptyOutDir: true,
-        manifest: true, // ensures Laravel can find the assets
-        rollupOptions: {
-            input: {
-                app: resolve(__dirname, 'resources/js/app.tsx'),
-            },
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: '127.0.0.1',
         },
+    },
+
+    esbuild: {
+        jsx: 'automatic',
     },
     resolve: {
         alias: {
@@ -65,3 +31,5 @@ export default defineConfig({
         },
     },
 });
+
+
