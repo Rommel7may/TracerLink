e4ebc9f807651e31555bf3bf683c273b3a3939bb
Rommel <2022307038@pampangastateu.edu.ph>
2025-09-18 16:51:50 +0800

    fixed broadcast

--- resources/js/echo.ts
+++ resources/js/echo.ts
@@ -1,11 +1,31 @@
+// import Echo from 'laravel-echo';
+
+// export const echo = new Echo({
+//   broadcaster: 'reverb',
+//   key: import.meta.env.VITE_REVERB_APP_KEY,
+//   wsHost: import.meta.env.VITE_REVERB_HOST || '127.0.0.1',
+//   wsPort: Number(import.meta.env.VITE_REVERB_PORT || 8080),
+//   wssPort: Number(import.meta.env.VITE_REVERB_PORT || 8080),
+//   forceTLS: import.meta.env.VITE_REVERB_SCHEME === 'https',
+//   enabledTransports: ['ws', 'wss'],
+// });
+
+
+
 import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
+
+declare global {
+    interface Window {
+        Pusher: any;
+    }
+}
+
+window.Pusher = Pusher;
 
 export const echo = new Echo({
-  broadcaster: 'reverb',
-  key: import.meta.env.VITE_REVERB_APP_KEY,
-  wsHost: import.meta.env.VITE_REVERB_HOST || '127.0.0.1',
-  wsPort: Number(import.meta.env.VITE_REVERB_PORT || 8080),
-  wssPort: Number(import.meta.env.VITE_REVERB_PORT || 8080),
-  forceTLS: import.meta.env.VITE_REVERB_SCHEME === 'https',
-  enabledTransports: ['ws', 'wss'],
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    forceTLS: true,
 });
