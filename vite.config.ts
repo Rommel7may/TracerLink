17807e935d257ccef089d00666d82718d079d390
Rommel <2022307038@pampangastateu.edu.ph>
2025-09-23 18:04:57 +0800

    yeye

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
+            input: ['resources/css/app.css', 'resources/js/app.tsx',
+                     'resources/js/pages/Students/Index.tsx'
+            ],
             ssr: 'resources/js/ssr.tsx',
             refresh: true,
         }),
